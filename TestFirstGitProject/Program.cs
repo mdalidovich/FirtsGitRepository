de01e0e369ab31a292097c7325eb6924f73b5527
Marina_Dalidovich <dalidovich.marina@gmail.com>
2020-01-21 10:24:51 +0200

    Default

---
+++ TestFirstGitProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestFirstGitProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
